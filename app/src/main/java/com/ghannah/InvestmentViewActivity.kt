661b0a322d0ef321764a75a9c6a10a5e6fb23b97
gamh86 <g.amh.86@outlook.fr>
2021-10-23 18:14:50 +0100

    Input values to text views

--- app/src/main/java/com/ghannah/InvestmentViewActivity.kt
+++ app/src/main/java/com/ghannah/InvestmentViewActivity.kt
@@ -1,6 +1,7 @@
 package com.ghannah
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class InvestmentViewActivity : AppCompatActivity()
@@ -9,6 +10,13 @@ class InvestmentViewActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
 
+        val inv : Investment = PortfoliusState.getCurrentlySelectedInvestment()
+
         setContentView(R.layout.view_investment)
+
+        findViewById<TextView>(R.id.textViewInvestmentAmountValue).setText(inv.getAmount().toString())
+        findViewById<TextView>(R.id.textViewInvestmentCostValue).setText(inv.getCost().toString())
+        findViewById<TextView>(R.id.textViewInvestmentRateValue).setText(inv.getRate().toString())
+        findViewById<TextView>(R.id.textViewInvestmentNetValue).setText(inv.net().toString())
     }
 }
