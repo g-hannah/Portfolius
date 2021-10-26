68390efd311b65a248d1f5f6226435a7bc52193c
gamh86 <g.amh.86@outlook.fr>
2021-10-26 21:56:55 +0100

    add overriding implementation of onRestart()

--- app/src/main/java/com/ghannah/ViewPortfolioActivity.kt
+++ app/src/main/java/com/ghannah/ViewPortfolioActivity.kt
@@ -3,52 +3,120 @@ package com.ghannah
 import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isEmpty
 import kotlinx.coroutines.selects.select
 
 class ViewPortfolioActivity : AppCompatActivity()
 {
-    override fun onCreate(savedInstanceState : Bundle?)
+    private var state : Bundle? = null
+    private var selectedCurrency : String? = null
+    private var confirmationDelete : Int = 0
+    /**
+     * Help function to display the investments
+     * for the currently selected portfolio
+     */
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showInvestments()
     {
-        super.onCreate(savedInstanceState)
+        val selectedPortfolio : Portfolio? = PortfoliusState.getCurrentlySelectedPortfolio()
 
-        val selectedPortfolio : Portfolio = PortfoliusState.getCurrentlySelectedPortfolio()
-
-        setContentView(R.layout.view_portfolio)
-
-        findViewById<TextView>(R.id.textViewPortfolioName)?.setText(selectedPortfolio._name)
+        if (null == selectedPortfolio)
+        {
+            Notification.error(this, "No selected portfolio")
+            finish()
+        }
 
         val ll : LinearLayout? = findViewById<LinearLayout>(R.id.linearLayoutPortfolioList)
-        val map : MutableMap<String,MutableList<Investment>> = selectedPortfolio.getInvestments()
+        val map : MutableMap<String,MutableList<Investment>> = selectedPortfolio!!.getInvestments()
         val res : Resources = applicationContext.resources
         val fmt : String = res.getString(R.string.investment_view_format_string)
 
-        if (0 == map.size)
+        /*
+         * We need to remove all the views from the layout
+         * as this method will be called from onRestart(),
+         * refilling the LinearLayout with TextViews of
+         * investments (so they would appear >1 time)
+         */
+        if (!ll?.isEmpty()!!)
+            ll?.removeAllViews()
+
+        if (map.isEmpty())
         {
             val tv = TextView(this)
-            tv.text = "No investments for this portfolio! Use the button below to add one!"
+            tv.text = applicationContext.resources.getString(R.string.no_investments_string)
             ll?.addView(tv)
         }
         else
         {
             for (key in map.keys)
             {
-                val tv = TextView(this)
+                val button = Button(this)
 
                 try
                 {
-                    tv.text = String.format(fmt, key, selectedPortfolio.netForCurrency(key))
+                    button.text = String.format(fmt, key, selectedPortfolio.netForCurrency(key))
+                    ll?.addView(button)
+
+                    button.setOnClickListener {
+
+                        if (it is Button)
+                        {
+                            val tmp = it.text.split(" ")
+                            this.selectedCurrency = tmp[0]
+                            PortfoliusState.setCurrentlySelectedInvestmentCurrency(tmp[0])
+                            startActivity(Intent(this, ViewInvestmentsActivity::class.java))
+                        }
+                    }
                 }
                 catch (e : NoInvestmentListForCurrencyException)
                 {
-                    Notification.send(this, e.message ?: "An error occurred...")
+                    Notification.error(this, e.message)
                 }
             }
         }
+
+        findViewById<Button>(R.id.buttonDeletePortfolio)
+            .setOnClickListener {
+
+                PortfolioManager.removePortfolioByName(selectedPortfolio._name)
+                Notification.send(this, "Removed portfolio")
+
+                finish()
+            }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        if (null == savedInstanceState)
+        {
+            Notification.send(this, "state is null")
+        }
+
+        val selectedPortfolio : Portfolio? = PortfoliusState.getCurrentlySelectedPortfolio()
+
+        if (null == selectedPortfolio)
+        {
+            Notification.error(this, "No portfolio is selected")
+            finish()
+        }
+
+        setContentView(R.layout.view_portfolio)
+
+        findViewById<TextView>(R.id.textViewPortfolioName)?.setText(selectedPortfolio!!._name)
+
+        showInvestments()
+
 
 //        for (i in 1..10)
 //        {
@@ -62,5 +130,13 @@ class ViewPortfolioActivity : AppCompatActivity()
 
                 startActivity(Intent(this, AddInvestmentActivity::class.java))
             }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onRestart()
+    {
+        super.onRestart()
+
+        showInvestments()
     }
 }
